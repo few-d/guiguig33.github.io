71b2b0a93e2c4049247e280206e302ef44e73ce5
GitHub few-d <GitHub 1579371990@qq.com>
2025-01-13 14:34:47 +0800

    Site updated: 2025-01-13 14:34:46

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -2,7 +2,7 @@
 const mobileBgImageUrl = "url('.../themes/fluid/source/img/default.png')";
 
 // 在电脑端显示的背景图片链接
-//const desktopBgImageUrl = "url('.../themes/fluid/source/img/default.png')";
+const desktopBgImageUrl = "url('.../themes/fluid/source/img/bs.png')";
 
 // 在手机端设置背景图片
 if (window.innerWidth < 768) {
@@ -13,9 +13,16 @@ if (window.innerWidth < 768) {
 }
 
 // 设置banner的背景图片为空
-document.querySelector("#banner").setAttribute('style', 'background-image: url(.../themes/fluid/source/img/bs.png)');
+document.querySelector("#banner").setAttribute('style', 'background-image: none');
 
 // 设置banner的.mask背景颜色透明
 document.querySelector("#banner .mask").setAttribute('style', 'background-color: rgba(0,0,0,0)');
 
-document.querySelector('#web_bg').setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+document
+
+    .querySelector('#web_bg')
+
+    .setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+
+
+
