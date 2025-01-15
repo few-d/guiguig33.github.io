05eb812c4a035fd79f661271e57e87e563629a08
GitHub few-d <GitHub 1579371990@qq.com>
2025-01-15 14:03:41 +0800

    Site updated: 2025-01-15 14:03:40

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -1,5 +1,5 @@
 // 在手机端显示的背景图片链接
-const mobileBgImageUrl = "url('/source/img/BG/bg.png')";
+const mobileBgImageUrl = "url('https://s21.ax1x.com/2025/01/14/pEiNM0x.jpg')";
 
 // 在电脑端显示的背景图片链接
 const desktopBgImageUrl = "url(https://s21.ax1x.com/2025/01/14/pEiNM0x.jpg)";
@@ -16,7 +16,7 @@ if (window.innerWidth < 768) {
     //document.querySelector('#web_bg').setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
 }
 // 设置banner的.mask背景颜色透明
-document.querySelector("#banner .mask").setAttribute('style', 'background-color: blueviolet;opacity: 0.5;');
+//document.querySelector("#banner .mask").setAttribute('style', 'background-color: blueviolet;opacity: 0.5;');
 
 
 
