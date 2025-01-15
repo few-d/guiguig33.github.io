6c974eff43d81e15cf0512dfe0facf6196987022
GitHub few-d <GitHub 1579371990@qq.com>
2025-01-15 09:10:20 +0800

    Site updated: 2025-01-15 09:10:19

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -1,19 +1,19 @@
 // 在手机端显示的背景图片链接
-const mobileBgImageUrl = "url('.../themes/fluid/source/img/default.png')";
+const mobileBgImageUrl = "url('/source/img/BG/bg.png')";
 
 // 在电脑端显示的背景图片链接
-const desktopBgImageUrl = "url('.../themes/fluid/source/img/bs.png')";
+const desktopBgImageUrl = "url('/source/img/BG/bg.png')";
 
 // 设置banner的背景图片为空
-document.querySelector("#banner").setAttribute('style', 'background-image: url(".../themes/fluid/source/img/bs.png")');
+document.querySelector("#banner").setAttribute('style', 'background-image: url("/source/img/BG/bg.png")');
 
 // 在手机端设置背景图片
 if (window.innerWidth < 768) {
     document.querySelector('#web_bg').setAttribute('style', `background-image: ${mobileBgImageUrl};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
 } else {
     // 在电脑端设置背景图片
-    //document.querySelector('#web_bg').setAttribute('style', `background-image: ${desktopBgImageUrl};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
-    document.querySelector('#web_bg').setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+    document.querySelector('#web_bg').setAttribute('style', `background-image: ${desktopBgImageUrl};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+    //document.querySelector('#web_bg').setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
 }
 // 设置banner的.mask背景颜色透明
 document.querySelector("#banner .mask").setAttribute('style', 'background-color: rgba(0,0,0,0)');
