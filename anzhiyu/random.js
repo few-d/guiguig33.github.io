26c486b8ce426d84e4989b7ce7f025bca89a2cf7
GitHub few-d <GitHub 1579371990@qq.com>
2025-01-17 17:29:39 +0800

    Site updated: 2025-01-17 17:29:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/05/10/dnoi/","2025/01/10/py_one/","2025/01/06/hello-world/","2023/05/10/html/","2025/01/09/post/"];function toRandomPost(){
+var posts=["2025/01/06/hello-world/","2023/05/10/dnoi/","2025/01/09/post/","2023/05/10/html/","2025/01/10/py_one/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
